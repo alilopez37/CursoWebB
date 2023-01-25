4b141e257018f8392c9917b320955bdc8d78a418
alilopez <ali.lopez37@gmail.com>
2023-01-25 09:46:40 -0600

    [ADD] estados

--- src/components/atoms/FormLogin.jsx
+++ src/components/atoms/FormLogin.jsx
@@ -1,13 +1,30 @@
+import {useState} from 'react'
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/react.svg'
 function FormLogin() {
+    //const [description, setDescription] = useState({msn: "CC", v : 1})
+    //const [username, setUsername] = useState('')
+    const [stateForm, setStateForm] = useState({username:'', password:''})
+    const handlerClick = (e) => {
+        e.preventDefault()
+        setDescription({msn: username})    
+    }
+    const handlerChange = (e) => {
+        setStateForm({...stateForm,username:e.target.value})
+    }
+    const handlerChangePassword = (e) => {
+        setStateForm({...stateForm,password: e.target.value})
+    }
     return (
         <form>
             <img src={Logo} alt="Logotipo de la empresa" />
-            <input type="text" />
-            <input type="password" />
-            <button>Iniciar sesión</button>
+            <input type="text" value={stateForm.username} onChange={handlerChange}/>
+            <input type="password" value ={stateForm.password} onChange={handlerChangePassword}/>
+            <button onClick={handlerClick}>Iniciar sesión</button>
             <Link to="/register">Registrate</Link>
+{            <div>
+                <label>{JSON.stringify(stateForm)}</label>
+            </div>}
         </form>
       );
 }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import FlexBox from '../components/molecules/FlexBox';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
+                <Route path='/flexbox' element={<FlexBox/>}/>
             </Routes>
         </BrowserRouter>
       );
