4aa0d5bdd222b36784dfb2d51cdd98ecffe2c8c4
alilopez <ali.lopez37@gmail.com>
2023-01-31 10:02:55 -0600

    [ADD] Uso de fetch

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,6 +3,7 @@ import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import FlexBox from '../components/molecules/FlexBox';
+import RickAndMorty from '../pages/RickAndMorty';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path="/login" element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
                 <Route path='/flexbox' element={<FlexBox/>}/>
+                <Route path='/rickandmorty' element={<RickAndMorty/>}/>
             </Routes>
         </BrowserRouter>
       );
